5f58c30353bf40df41b7396b8d2457e945a5b875
Ashraf Elshazly <60497722+AshrafElshazly@users.noreply.github.com>
2024-01-23 06:35:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 // Redirect root to
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, this is a sample app!</h1> <br> <p>Fetch users from the database by visiting <a href="/users">users</a><p>.');
+  res.send('<h1>Hello, this is a sample app!</h1> <br> <p>Fetch users from the database by visiting <a href="/users">users</a><p> <br> update from jenkins.');
 });
 
 app.get('/users', (req, res) => {
